74657c1c8a1e7f66290fd28f4f582250f021c60f
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2023-02-18 10:52:28 -0500

    quotes syntax

---
+++ src/components/signup-view/signup-view.jsx
@@ -0,0 +1,83 @@
+import { useState } from 'react';
+
+export const SignUp = () => {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [birthday, setBirthday] = useState('');
+    //input will be a string, so state starts as empty string
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //prevents default behavior of page reloading every time a field is submitted. default form behavior it to reload the entire page with submit.
+
+        const data = {
+            Name: username,
+            Password: password,
+            Email: email,
+            Birthday: birthday
+        };
+
+        fetch('https://sophia-films.herokuapp.com/users', {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+            //tells the server that the data being sent in the request is json so that the server can properly parse the data
+        }).then((response) => {
+            if(response.ok) {
+                alert('Signup successful!');
+                window.location.reload();
+            } else {
+                alert('Signup failed.')
+            }
+        })
+    };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type='text'
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+          minLength='3'
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          minLength='8'
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          type='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Birthday:
+        <input
+          type='date'
+          value={birthday}
+          onChange={(e) => setBirthday(e.target.value)}
+        />
+      </label>
+      <button type='submit'>Submit</button>
+    </form>
+  );
+};
+
+
